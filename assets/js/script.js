8643562aaf72f08bdea7282eaa28119db6603a0b
AlejandroOFDY <BB_Rae@hotmail.com>
2025-05-29 16:42:20 -0300

    Actualizando los archivos con lo que pasó el profesor antes de que terminé la clase

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-fetch()
+const dbz_api = "https://web.dragonball-api.com";
+const btnBuscar = document.getElementById("btn-buscar");
+const contenedorPadre = document.getElementById("contenedor-data");
+
+/* fetch("https://web.dragonball-api.com")
 .then(fdxponxd => Response.json())
     if (!Response.OK){
         throw new Error("Error de la Api")
     }
     return Response.json()
 .then((data) => console.log(data.items))
-.cath((error) => console.log(error))
+.cath((error) => console.log(error)) */
 
 const cargarDatos = async () => {
     try {
-        const response = await feth("https://web.dragonball-api.com");
+        const response = await feth(dbz_api);
         if (response.ok) {
             throw new error("Error de la Api")
         }
